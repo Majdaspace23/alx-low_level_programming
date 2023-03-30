628ab0e7dfd2cba435344b232bae2d4978d75288
Majdaspace23 <meh.majda@outlook.fr>
2023-03-30 03:24:53 -0700

    printfc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 /**
-#include<stdio.h>
-/**
 * main - Entry point
 *
 * Return: Always 0 (Success)
@@ -11,4 +9,3 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-* main - Entry point
